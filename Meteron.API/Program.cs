4f43c5c48a8d20349dd0dc12b518f343bb9db652
freedom-matthew <svobodamatej97@gmail.com>
2025-09-25 20:26:55 +0200

    Delete unused repository, update endpoints

--- Meteron.API/Program.cs
+++ Meteron.API/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Place SQLite DB file in the same project as MeteronDbContext (Infrastructure)
 var infrastructureRoot = Path.GetFullPath("../Meteron.Infrastructure", builder.Environment.ContentRootPath);
 var dbPath = Path.Combine(infrastructureRoot, "meteron.db");
 Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
@@ -13,14 +12,12 @@ var connectionString = $"Data Source={dbPath}";
 
 Console.WriteLine($"[EF] SQLite DB path: {dbPath}");
 
-builder.Services.AddDbContext<MeteronDbContext>(options =>
-    options.UseSqlite(connectionString, x => x.MigrationsAssembly("Meteron.Infrastructure")));
+builder.Services.AddDbContext<MeteronDbContext>(options => options.UseSqlite(connectionString, x => x.MigrationsAssembly("Meteron.Infrastructure")));
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -34,19 +31,21 @@ List<CustomerResponse> customers = [];
 
 app.MapGet("/customers", () => customers);
 
-app.MapPost("/customers/household", (CreateHouseholdCustomerRequest request) =>
+app.MapPost("/customers/household", async (CreateHouseholdCustomerRequest request, MeteronDbContext db) =>
 {
     var customer = HouseholdCustomer.Create(new Email(request.Email), request.FirstName, request.LastName, request.DateOfBirth);
+    await db.Customers.AddAsync(customer);
+    
     customers.Add(customer.ToResponse());
-
     return customer.ToResponse();
 });
 
-app.MapPost("/customers/business", (CreateBusinessCustomerRequest request) =>
+app.MapPost("/customers/business", async (CreateBusinessCustomerRequest request, MeteronDbContext db) =>
 {
     var customer = BusinessCustomer.Create(new Email(request.Email), request.Name, request.RegistrationNumber);
-    customers.Add(customer.ToResponse());
+    await db.Customers.AddAsync(customer);
     
+    customers.Add(customer.ToResponse());
     return customer.ToResponse();
 });
 
